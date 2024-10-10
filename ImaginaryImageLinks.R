cd8d12f229100b2ba864d3f49079973f6db7dc32
Ryan A. Folk <folk.41@buckeyemail.osu.edu>
2024-10-10 09:03:22 -0500

    remapping of all Bisque URLs to Imaginary URLs and fix documentation in the script

--- ImaginaryImageLinks.R
+++ ImaginaryImageLinks.R
@@ -1,55 +1,60 @@
 # createImageLinks from CyVerse Imaginary service
-# author Herrick H.K. Brown
-# 2022-11-09
-# code modified on 2023-05-06 (CC) to fix spelling error of directory from "SCotty" to "Scotty"
+# author Herrick H.K. Brown 2022-11-09
+# Edits by Ryan Folk 2024 forward, variables, paths &c.
 
-# vars to modify
+# Only this code block should need to be modified, and really only date needs to be modified in each mapping file creation
+# It is not necessary to set the working directory
 
-# enter root directory for image folder
-rootdir <- "/Users/ryanfolk/Desktop/cyverse_imaginary/temp/" # only absolute paths here
-# enter upload date, needs to match dated folder name
-date <- "2024-7-8"
-# build working dir
+# IMPORTANT TIPS:
+# MAKE SURE LOCAL FILES ARE IN DATED FOLDER (if you've already deleted a local temporary copy you can create a sequential batch of files of zero bytes like so and the script will still work:
+# for i in {33102..34506}; do touch MISSA0${i}.JPG; done
+# Before submitting to the SERNEC website, check a couple of the URLs in the CSV result to make sure they actually resolve to an image.
+# ALWAYS CHECK SOME IMAGE URLs BEFORE SUBMITTING -- just paste them in your browser
+# Submission area in Symbiota is Administration Control Panel > Processing Toolbox > New Profile > Image URL Mapping File then upload the CSV
+# Make sure to check the box in the next screen that says to create new records if existing records are not found
+
+### Set paths
+# Enter root directory for image folder
+rootdir <- "/Users/ryanfolk/Documents/GitHub/cyverse_imaginary/temp/" # only absolute paths here
+# Enter upload date, needs to match dated folder name including any extra bits
+date <- "3-27-2023_missed2017-2018_photos"
+# Build working directory
 wkdir <- paste(rootdir,date,sep="")
-# check that the path exists
+# Check that the working directory path exists
 dir.create(file.path(paste(rootdir, date, "/", sep="")))
-# output location
-fiOUT <- "/Users/ryanfolk/Desktop/cyverse_imaginary/temp/"
+# Set output location
+fiOUT <- "/Users/ryanfolk/Documents/GitHub/cyverse_imaginary/temp/"
 
-# RAF: MAKE SURE LOCAL FILES ALSO IN DATED FOLDER
-# make sure there are not any CSVs already in the folder
-
-# stuff that actually does stuff
-
-# build file list including directory
-FL <- sub("/Users/ryanfolk/Desktop/cyverse_imaginary/temp/", "MISSA/", dir(path = wkdir, full.names=TRUE))
-# get file names from file list
+### Create mappings
+# Build file list including directory
+FL <- sub("/Users/ryanfolk/Documents/GitHub/cyverse_imaginary/temp/", "MISSA/", dir(path = wkdir, full.names=TRUE))
+# Get file names from file list
 catnum<-sub(".*/", "", FL)
-# remove .JPG from file names
+# Remove .JPG from file names
 catnum<-sub(".JPG", "", catnum, ignore.case=TRUE)
-# strip off "_letter" from file names
-# so that only catalogNumber remains
-# changed it to be case insensitive on 2023-11-20 (HB)
+# Strip off "_letter" from file names
+# So that only catalogNumber remains
+# Changed it to be case insensitive on 2023-11-20 (HB)
 # b/c some files have ".jpg" vs. ".JPG"
 catnum <- gsub("_\\D+","",catnum,ignore.case=TRUE)
-#changed from "_\\D",""..." to "_\\D+",""..." on 2023-11-20 (HB).
-#realized there was a problem when adding in situ images to the workflow because
-#USCH0068840_insitu was changed to USCH0068400nsitu
-#adding "+" matches 1 to unlimited times as many as needed
+# Changed from "_\\D",""..." to "_\\D+",""..." on 2023-11-20 (HB).
+# Realized there was a problem when adding in situ images to the workflow because
+# USCH0068840_insitu was changed to USCH0068400nsitu
+# Adding "+" matches 1 to unlimited times as many as needed
+# These patterns should not pertain to MISSA but still keep them
 
-#create dataFrame
+# Create empty dataframe
 df <- data.frame(matrix(ncol = 5, nrow = 0))
-# assign column names to match images.csv in DwC-A format
+# Assign a vector of column names to match images.csv in DwC-A format
+# Symbiota should automatically recognize and map these column headers in the web interface
 x <- c("catalogNumber","sourceURL","originalURL","thumbnailURL","URL")
-# insert colnames into df
+# Insert column names into the dataframe
 colnames(df)<-x
-# populate df with constructed URLs and corresponding catalogNumbers
-df[1:length(FL),]<-c(catnum,paste("https://img.cyverse.org/resize?width=4000&url=https://data.cyverse.org/dav-anon/iplant/projects/sernec/",FL,sep=""),
-                           paste("https://img.cyverse.org/resize?width=4000&url=https://data.cyverse.org/dav-anon/iplant/projects/sernec/",FL,sep=""),
-                           paste("https://img.cyverse.org/thumbnail?height=200&url=https://data.cyverse.org/dav-anon/iplant/projects/sernec/",FL,sep=""),
-                           paste("https://img.cyverse.org/resize?width=1250&url=https://data.cyverse.org/dav-anon/iplant/projects/sernec/",FL,sep=""))
+# Populate the dataframe with constructed Imaginary URLs and corresponding catalogNumbers
+df[1:length(FL),]<-c(catnum,paste("https://img.cyverse.org/resize?width=4000&url=https://data.cyverse.org/dav-anon/iplant/projects/magnoliagrandiFLORA/sernec-portal/",FL,sep=""),
+                           paste("https://img.cyverse.org/resize?width=4000&url=https://data.cyverse.org/dav-anon/iplant/projects/magnoliagrandiFLORA/sernec-portal/",FL,sep=""),
+                           paste("https://img.cyverse.org/thumbnail?height=200&url=https://data.cyverse.org/dav-anon/iplant/projects/magnoliagrandiFLORA/sernec-portal/",FL,sep=""),
+                           paste("https://img.cyverse.org/resize?width=1250&url=https://data.cyverse.org/dav-anon/iplant/projects/magnoliagrandiFLORA/sernec-portal/",FL,sep=""))
+# Write the mapping file
+write.csv(df, paste(fiOUT, "UpLd_", date, "_Fldr_", date, "_", min(catnum), "-", max(catnum), ".csv", sep=""), row.names = FALSE)
 
-write.csv(df, paste(fiOUT, "UpLd_", date, "_Fldr_", JPGfldr, "_", min(catnum), "-", max(catnum), ".csv", sep=""), row.names = FALSE)
-
-# Before submitting to the SERNEC website, check a couple of the URLs in the CSV result to make sure they actually resolve to an image.
-# ALWAYS CHECK SOME IMAGE LINKS
